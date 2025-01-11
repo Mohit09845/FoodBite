764acc262fc23843ae7245513e612b9c1f70440a
Mohit Sharma <sharmaamohit121@gmail.com>
2025-01-11 18:48:14 +0530

    server setup

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express,{Request,Response} from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/test',async(req: Request,res: Response)=>{
+    res.json({
+        message: "Hello"
+    })
+})
+
+app.listen(8080,()=>{
+    console.log('Server started on 8080');
+})
